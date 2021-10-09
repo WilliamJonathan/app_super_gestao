c955ed3a0b1471ed633ce7440f7db86c0ecb89f6
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-09 18:23:53 -0300

    primeiras rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,13 @@ use Illuminate\Support\Facades\Route;
 php artisan make:controller NomeControladorController
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-
-Route::get('/contato/{nome}', function(string $nome) {
-    echo 'Estamos aqui: '.$nome;
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
