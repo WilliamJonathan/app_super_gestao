cd112d593b426669b52a8786e7451beec2572235
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-10 14:23:19 -0300

    rota de fallback configurada

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,13 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
+//grupo de rotas
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
     Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para pagina inicial';
 });
