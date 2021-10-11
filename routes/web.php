03577c26c2d7e08d9c81f253bb0a96208bfe9935
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-11 10:23:31 -0300

    implementado method post de teste

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ php artisan make:controller NomeControladorController
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 //grupo de rotas
