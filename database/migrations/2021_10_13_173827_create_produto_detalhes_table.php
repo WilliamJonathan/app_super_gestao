455790a75fb94265e77c0ba24314276e13fb8b61
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-13 14:50:04 -0300

    relacionamento 1 pra 1
    Primeiro migrate configurando um relacionamento 1 pra 1

---
+++ database/migrations/2021_10_13_173827_create_produto_detalhes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //declaração das colunas
+            $table->id();
+            $table->unsignedBigInteger('produto_id');//fk da tabela produtos
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+            //declaração da FK
+            //relacionamento 1 pra 1
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
