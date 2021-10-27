00b96f040aaa32eebdfee6f21c943811e5ad47be
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-27 10:11:38 -0300

    primeiro teste com tiker create fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //atributo table para setar nome da tabela no banco
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
