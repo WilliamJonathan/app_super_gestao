2567a56dba6f1e605d2b576b7a3410010ef71156
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-10-28 14:40:37 -0300

    primeiro teste com softDelete

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Fornecedor extends Model
 {
     //atributo table para setar nome da tabela no banco
+    use softDeletes;
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
