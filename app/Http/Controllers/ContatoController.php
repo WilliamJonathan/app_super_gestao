5bd63d18a02e703c98ae8b6493e3870ac9b68254
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-06 14:55:30 -0300

    persistir dados via formulario

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        //var_dump($_POST);
+    public function contato(Request $request) {
+        /*$contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();*/
+
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+
+        //print_r($contato->getAttributes());
+
         return view('site.contato');
     }
 }
