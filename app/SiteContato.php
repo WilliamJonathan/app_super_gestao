65515eda8ba0145081b3827ff362fa1b10857577
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-08 13:59:03 -0300

    .ajuste

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -32,5 +32,5 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //
-    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contatos_id', 'mensagem'];
 }
