e0371f080ea073fb8c279200590e2ce8f16016fd
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-08 10:33:27 -0300

    .ajuste

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,21 +8,14 @@ use App\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-        /*$contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-        $contato->save();*/
 
-        //$contato = new SiteContato();
-        //$contato->fill($request->all());
-        //$contato->save();
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
 
-        //print_r($contato->getAttributes());
-
-        return view('site.contato');
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
