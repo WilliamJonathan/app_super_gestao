ed4e2f2ca37160ab31ad303fad1bb59fbc5c426f
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-08 15:18:34 -0300

    Customizando feedbacks

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,23 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
         //validar os dados recebidos pelo formulario
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000'
-        ]);
+        $request->validate(
+            [
+                'nome' => 'required|min:3|max:40',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000'
+            ],
+            [
+                'required' => 'O campo precisa ser requerido',
+                'nome.required' => 'O campo nome precisa ser requerido',
+                'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome pode ter no maximo 40 caracteres',
+                'telefone.required' => 'O campo telefone precisa ser requerido',
+                'email' => 'E-mail informado invalido'
+            ]
+        );
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
