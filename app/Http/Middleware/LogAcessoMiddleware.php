db48a29a1f4240be52b89f8adf87aa4ebe1d8458
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-09 10:04:57 -0300

    atribuido middleware LogAcesso a todas as rotas

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -22,9 +22,9 @@ class LogAcessoMiddleware
 
     public function handle($request, Closure $next)
     {
-        $ip = $request->server->get('REMOTE_ADDR');
-        $rota = $request->getRequestUri();
-        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
+        //$ip = $request->server->get('REMOTE_ADDR');
+        //$rota = $request->getRequestUri();
+        //LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +17,7 @@ use App\Http\Middleware\LogAcessoMiddleware;
 php artisan make:controller NomeControladorController
 */
 
-Route::middleware(LogAcessoMiddleware::class)
-    ->get('/', 'PrincipalController@principal')
-    ->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
